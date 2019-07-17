790e0eb2b92775fc969de6b5526359b6fdc5e038
qiubo <103095649@qq.com>
2019-07-17 08:39:45 +0800

    页面加载完成后再显示窗口

--- src/basic/index.js
+++ src/basic/index.js
@@ -5,8 +5,8 @@ function createWindow() {
     // width height minWidth  maxWidth默认单位px
     //x y 在屏幕上的位置
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         webPreferences: {
             nodeIntegration: true
         },
@@ -18,11 +18,16 @@ function createWindow() {
         // kiosk: true
         //title 如果在网页中设置了，这里无效
         title: 'aaaa',
-        icon: '../img/icon.png'
+        icon: '../img/icon.png',
+        show: false
     });
 
     // 加载index.html文件
     win.loadFile('index.html');
+    win.on('ready-to-show', () => {
+        win.show();
+    });
+
     // 显示调试窗口
     win.webContents.openDevTools();
 
